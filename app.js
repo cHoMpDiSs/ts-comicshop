7c1ec254f61f062d6d69fc2f67318cfd6f56605e
Jordon Marchesano <jordonomarchesano@gmail.com>
2023-10-30 10:15:51 -0700

    heroku 1.5 cors update 12

--- app.js
+++ app.js
@@ -22,10 +22,7 @@ const cors_1 = __importDefault(require("cors"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = parseInt(process.env.PORT || '3000', 10);
-const allowedOrigins = ['http://localhost:3000', 'https://ts-comicshop-28397a67a86b.herokuapp.com'];
-app.use((0, cors_1.default)({
-    origin: allowedOrigins
-}));
+app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.use(body_parser_1.default.urlencoded({
     extended: true,
