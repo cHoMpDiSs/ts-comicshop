32ecc0faa1e5a0a7a01aba382351c593759c297e
Jordon Marchesano <jordonomarchesano@gmail.com>
2023-10-30 09:58:58 -0700

    heroku 1.5 cors update 8

--- app.js
+++ app.js
@@ -23,8 +23,6 @@ const app = (0, express_1.default)();
 const port = parseInt(process.env.PORT || '3000', 10);
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
     next();
 });
 // app.use(cors());
--- app.ts
+++ app.ts
@@ -16,8 +16,6 @@ const port: number = parseInt(process.env.PORT || '3000', 10);
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
 })
 
